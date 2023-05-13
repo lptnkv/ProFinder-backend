0e890bce2e8f08cf47b091959157fb5c655d9176
lptnkv <lipatnikov.ks@gmail.com>
2023-05-13 17:19:53 +0300

    prepare to transfer to other workplace(macbook)

---
+++ server.mjs
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/record", records)
